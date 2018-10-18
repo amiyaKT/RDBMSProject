cb31c14507b277b7924180d1a3d1f2b785efbeed
amiya-1998 <amiya.tripathy1998@gmail.com>
2018-10-18 09:54:39 +0530

    TESTING DB

--- database/database.js
+++ database/database.js
@@ -1,11 +1,8 @@
 const pg = require('pg');
 
 const pool = new pg.Pool({
-  user: 'jedysgnrdmbeml',
-  host: 'ec2-23-21-147-71.compute-1.amazonaws.com',
-  database: 'dts9qm174n3m',
-  password: 'c6f338dece3076829650f048297e584fb3196e667b772caea9d1fa9b01da7925',
-  port: '5432'
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
 });
 
 module.exports = pool;
