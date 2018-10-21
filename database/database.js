5e81803e28f120aa08a10640b4065ab1a8fd2156
amiya-1998 <amiya.tripathy1998@gmail.com>
2018-10-21 19:39:58 +0530

    Rating System Added. History table Schema Updated.

--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const pg = require('pg');
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   // Use the below Connection String for developement purposees and the above in heroku environment
-  // connectionString:'postgres://jedysgnrdmbeml:c6f338dece3076829650f048297e584fb3196e667b772caea9d1fa9b01da7925@ec2-23-21-147-71.compute-1.amazonaws.com:5432/dts9qm174n3m',
+  // connectionString:
+  //   'postgres://jedysgnrdmbeml:c6f338dece3076829650f048297e584fb3196e667b772caea9d1fa9b01da7925@ec2-23-21-147-71.compute-1.amazonaws.com:5432/dts9qm174n3m',
   ssl: true
 });
 
--- routes/books.js
+++ routes/books.js
@@ -5,6 +5,7 @@ const express = require('express'),
 
 // BOOKS ROUTES
 
+// Show all Books
 router.get('/', (req, res) => {
   pool.query(
     `SELECT b.id, b.ISBN, b.title, b.title, b.author, b.publisher, b.image_url, b.rating, g.name FROM books b, genre g WHERE g.id = b.genre`,
@@ -18,17 +19,18 @@ router.get('/', (req, res) => {
   );
 });
 
+// Show add new book form for admin
 router.get('/new', middleware.checkIsAdmin, (req, res) => {
   pool.query(`SELECT * FROM genre`, (err, response) => {
     if (err) {
       console.log(err);
     } else {
-      console.log(response.rows);
       res.render('./books/new', { genres: response.rows });
     }
   });
 });
 
+// Logic to add a new book to the database via the add the new book form
 router.post('/', middleware.checkIsAdmin, (req, res) => {
   if (req.body.book.image_url === '') {
     req.body.book.image_url =
@@ -52,6 +54,7 @@ router.post('/', middleware.checkIsAdmin, (req, res) => {
   );
 });
 
+// Show description and extra details about a particular book
 router.get('/:id', (req, res) => {
   pool.query(
     `SELECT b.id, b.ISBN, b.title, b.title, b.author, b.publisher, b.image_url, b.description, b.rating, g.name AS genre FROM books b, genre g WHERE g.id = b.genre AND b.id = ${
@@ -68,6 +71,7 @@ router.get('/:id', (req, res) => {
   );
 });
 
+// Edit book details form for admin
 router.get('/edit/:id', middleware.checkIsAdmin, (req, res) => {
   pool.query(`SELECT * FROM genre`, (err, response_genre) => {
     if (err) {
@@ -92,19 +96,96 @@ router.get('/edit/:id', middleware.checkIsAdmin, (req, res) => {
   });
 });
 
+// Logic to update book details
 router.put('/:id', middleware.checkIsAdmin, (req, res) => {
   res.send('UPDATE Book Details Logic');
 });
 
+// Delete a particular book from database
 router.delete('/:id', middleware.checkIsAdmin, (req, res) => {
   let id = req.params.id;
-  pool.query(`DELETE FROM books WHERE id = ${id}`, (err, response) => {
+  pool.query(`DELETE FROM history WHERE book_id = ${id}`, err => {
     if (err) {
       console.log(err);
     } else {
-      res.redirect('/books');
+      pool.query(`DELETE FROM ratings WHERE book_id = ${id}`, err => {
+        if (err) {
+          console.log(err);
+        } else {
+          pool.query(`DELETE FROM books WHERE id = ${id}`, err => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.redirect('/books');
+            }
+          });
+        }
+      });
     }
   });
+});
+
+// Purchase a specific book
+router.post('/purchase/:id', middleware.isAuthenticated, (req, res) => {
+  pool.query(
+    `INSERT INTO history(user_id, book_id) VALUES(${
+      res.locals.currentUser.id
+    }, ${req.params.id})`,
+    (err, response) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.redirect(`/books/rate/${req.params.id}`);
+      }
+    }
+  );
+});
+
+// User will rate a book here
+router.get('/rate/:id', middleware.isAuthenticated, (req, res) => {
+  pool.query(
+    `SELECT * FROM ratings WHERE user_id = ${
+      res.locals.currentUser.id
+    } AND book_id = ${req.params.id}`,
+    (err, response) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (response.rows.length > 0) {
+          res.redirect('/books');
+        } else {
+          res.render('./books/rate', { book_id: req.params.id });
+        }
+      }
+    }
+  );
+});
+
+// User rating logic
+router.post('/rate/:id', middleware.isAuthenticated, (req, res) => {
+  pool.query(
+    `INSERT INTO ratings VALUES(${res.locals.currentUser.id}, ${
+      req.params.id
+    }, ${req.body.rating})`,
+    (err, response) => {
+      if (err) {
+        console.log(err);
+      } else {
+        pool.query(
+          `UPDATE books SET rating = (SELECT AVG(rating) FROM ratings WHERE book_id = ${
+            req.params.id
+          }) WHERE id = ${req.params.id}`,
+          (err, response) => {
+            if (err) {
+              console.log(err);
+            } else {
+              res.redirect('/books');
+            }
+          }
+        );
+      }
+    }
+  );
 });
 
 module.exports = router;
