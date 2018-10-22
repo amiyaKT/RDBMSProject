ca327abc42e198486ad349b63c01b2d86b09fc34
amiya-1998 <amiya.tripathy1998@gmail.com>
2018-10-22 11:52:53 +0530

    Minor UI bug fixes for mobile devices.

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,20 @@
+// Used to hide the toggleable navbar on clicking anywhere outside it.
 $(document).on('click', function() {
   $('.collapse').collapse('hide');
 });
 
+// Used for the dropdown in addd books section for admin.
 $('.ui.dropdown').dropdown({
   forceSelection: false
 });
+
+// Used to show the ratings on each book
 $('.rating').rating({
   maxRating: 5,
   interactive: false
 });
+
+// Used to enable interaction with the user to rate the book
 $('#userRating').rating({
   maxRating: 5,
   interactive: true,
