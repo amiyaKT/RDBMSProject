6bb1eac5eac22be590d59c9e42008f4ee86f13a4
amiya-1998 <amiya.tripathy1998@gmail.com>
2018-10-25 19:50:38 +0530

    Edit comment modal bug fix.

--- routes/cart.js
+++ routes/cart.js
@@ -5,26 +5,34 @@ const express = require('express'),
 
 // view all cart content
 
-router.get("views", middleware.isAuthenticated, (req,res)=>{
-  pool.query(`SELECT c.id AS id, b.title AS title, c.qty AS qty FROM books b, cart c WHERE c.user_id = ${res.locals.currentUser.id} AND b.id = c.book_id`, (err, res_cart)=>{
-    if(err){
-      console.log(err);
-    } else {
-      res.render("cart",{cart: res_cart.rows});
+router.get('/views', middleware.isAuthenticated, (req, res) => {
+  pool.query(
+    `SELECT c.id AS id, b.title AS title, c.qty AS qty FROM books b, cart c WHERE c.user_id = ${
+      res.locals.currentUser.id
+    } AND b.id = c.book_id`,
+    (err, res_cart) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render('cart', { cart: res_cart.rows });
+      }
     }
-  });
+  );
 });
 
 // manual remove from cart
 
-router.post("/:id/del",middleware.isAuthenticated ,(req,res)=>{
-  pool.query(`DELETE FROM cart WHERE id = ${req.params.id}`, (err, res_cart)=>{
-    if(err){
-      console.log(err);
-    } else {
-      res.redirect("/cart/views");
+router.post('/:id/del', middleware.isAuthenticated, (req, res) => {
+  pool.query(
+    `DELETE FROM cart WHERE id = ${req.params.id}`,
+    (err, res_cart) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect('/cart/views');
+      }
     }
-  });
+  );
 });
 
 module.exports = router;
--- routes/comment.js
+++ routes/comment.js
@@ -28,31 +28,38 @@ router.post('/:id', middleware.isAuthenticated, (req, res) => {
 
 // UPDATE COMMENT
 
-router.post("/:id/edit/:book", middleware.isAuthenticated, (req, res) => {
-  req.body.comm_edited = req.body.comm_edited.replace(
+router.put('/:book/:id', middleware.isAuthenticated, (req, res) => {
+  req.body.comment = req.body.comment.replace(
     new RegExp('\r?\n', 'g'),
     '<br />'
   );
-  pool.query(`UPDATE comments SET comment = $$${req.body.comm_edited}$$ WHERE id = ${req.params.id}` , (err, response)=>{
-    if(err){
-      console.log(err);
-    } else {
-      res.redirect(`/books/${req.params.book}`);
+  pool.query(
+    `UPDATE comments SET comment = $$${req.body.comment}$$ WHERE id = ${
+      req.params.id
+    }`,
+    (err, response) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect(`/books/${req.params.book}`);
+      }
     }
-  });
+  );
 });
 
 // DELETE COMMENT
 
-router.post("/:id/del/:book", middleware.isAuthenticated, (req, res) => {
-  pool.query(`DELETE FROM comments WHERE id = ${req.params.id}` , (err, response)=>{
-    if(err){
-      console.log(err);
-    } else {
-      res.redirect(`/books/${req.params.book}`);
+router.delete('/:id/:book', middleware.isAuthenticated, (req, res) => {
+  pool.query(
+    `DELETE FROM comments WHERE id = ${req.params.id}`,
+    (err, response) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.redirect(`/books/${req.params.book}`);
+      }
     }
-  });
+  );
 });
-
 
 module.exports = router;
