77697e7bb97dee619ca9173fa20d6aac43d63212
amiya-1998 <amiya.tripathy1998@gmail.com>
2018-10-31 23:10:16 +0530

    Added loading animation

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,44 @@
-// Used to hide the toggleable navbar on clicking anywhere outside it.
-$(document).on('click', function() {
-  $('.collapse').collapse('hide');
+$(document).ready(function() {
+  // Used to hide the toggleable navbar on clicking anywhere outside it.
+  $(document).on('click', function() {
+    $('.collapse').collapse('hide');
+  });
+
+  // Used for the dropdown in addd books section for admin.
+  $('.ui.dropdown').dropdown({
+    forceSelection: false
+  });
+
+  // Used to show the ratings on each book
+  $('.rating').rating({
+    maxRating: 5,
+    interactive: false
+  });
+
+  // Used to enable interaction with the user to rate the book
+  $('#userRating').rating({
+    maxRating: 5,
+    interactive: true,
+    onRate: function(rating) {
+      $('#ratingInput').attr('value', rating);
+    }
+  });
+
+  // Validate Form
+  $('#register').form({
+    fields: {
+      firstname: 'empty',
+      lastname: 'empty',
+      username: 'empty',
+      email: 'empty',
+      address: 'empty',
+      password: 'empty'
+    }
+  });
+  setTimeout(removeLoader, 2000);
 });
 
-// Used for the dropdown in addd books section for admin.
-$('.ui.dropdown').dropdown({
-  forceSelection: false
-});
-
-// Used to show the ratings on each book
-$('.rating').rating({
-  maxRating: 5,
-  interactive: false
-});
-
-// Used to enable interaction with the user to rate the book
-$('#userRating').rating({
-  maxRating: 5,
-  interactive: true,
-  onRate: function(rating) {
-    $('#ratingInput').attr('value', rating);
-  }
-});
-
-// Validate Form
-$('#register').form({
-  fields: {
-    firstname: 'empty',
-    lastname: 'empty',
-    username: 'empty',
-    email: 'empty',
-    address: 'empty',
-    password: 'empty'
-  }
-});
+const removeLoader = () => {
+  $('#loadingMask').fadeOut('fast');
+  $('section').fadeIn('slow');
+};
