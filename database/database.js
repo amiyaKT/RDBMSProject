f0b42a81143f94cdce4110fe0190f418bae34ff1
amiya-1998 <amiya.tripathy1998@gmail.com>
2018-11-01 23:33:44 +0530

    Changes

--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const pg = require('pg');
 
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString:
+    'postgres://jedysgnrdmbeml:c6f338dece3076829650f048297e584fb3196e667b772caea9d1fa9b01da7925@ec2-23-21-147-71.compute-1.amazonaws.com:5432/dts9qm174n3m',
   ssl: true
 });
 
