f67a54ff981272f57dd9789aa3c5573aed706054
agentmonarch <monarchk3note@gmail.com>
2018-11-06 18:28:51 +0530

    Done(Except linking with node)

--- scripts/recommender.py
+++ scripts/recommender.py
@@ -19,3 +19,45 @@ rows = cursor.fetchall()
 data = pd.DataFrame(rows, columns=['user','book','rating'])
 user_rating = pd.pivot_table(data = data, values = 'rating', index='user', columns='book')
 correlation_matrix = user_rating.corr(method='pearson')
+
+#Getting the ratings given by the user
+cursor.execute("SELECT * FROM ratings WHERE user_id = 18;")
+ratings = cursor.fetchall()
+
+#Getting the book list
+cursor.execute("SELECT id FROM books;")
+books = cursor.fetchall()
+
+#creating the book_correlation_values list
+book_correlation_values = []
+for book in books:
+    i = book
+    book_correlation_values.append([i[0], 0])
+    
+#Iterating on the ratings list and computing correlation value between the book and other books and then multipling it with the rating and adding into book_correlation_values list
+for tup in ratings:
+    user, book, rating = tup
+    
+    # getting the users who rated this particular book and making sure rating is not zero
+    OneManOut_rating = user_rating[book]
+    
+    # finidng similarity using Pearson correlation
+    similar_to_OneManOut = user_rating.corrwith(OneManOut_rating)
+    corr_OneManOut = pd.DataFrame(similar_to_OneManOut, columns=['PearsonR'])
+    corr_OneManOut.dropna(inplace=True)
+    
+    # getting the most similar book
+    corr_OneManOut_sorted = corr_OneManOut.sort_values('book', ascending=True)
+    
+    
+    for i in range(0, len(book_correlation_values)):
+        
+        book_correlation_values[i][1] += corr_OneManOut_sorted.iloc[i][0]
+
+book_correlation_values_df = pd.DataFrame(book_correlation_values)
+book_correlation_values_sorted = book_correlation_values_df.sort_values(1, ascending=False)
+recommended_books = []
+for i in range(0, len(book_correlation_values)):
+    recommended_books.append(int(book_correlation_values_sorted.iloc[i][0]))
+
+    
